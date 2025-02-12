b514a23633a72b0c999dfc963f4482835ff98261
Okman-0920 <nohoksun94@gmail.com>
2025-02-12 11:21:24 +0900

    if

--- src/test/kotlin/ControlFlow.kt
+++ src/test/kotlin/ControlFlow.kt
@@ -1,7 +1,16 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class ControlFlow {
 
     // if
+    @Test
+    fun `if`() {
+        val max = max(1, 10)
+        assertEquals(10, max)
+    }
 
+    private fun max(num1: Int, num2: Int): Int = /* return 타입 */ if (num1 > num2) num1 else num2
 
     // when
 
