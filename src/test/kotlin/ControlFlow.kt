68edc9745d09024a9adbfab605114fea51ad937f
Okman-0920 <nohoksun94@gmail.com>
2025-02-12 11:24:20 +0900

    when 1

--- src/test/kotlin/ControlFlow.kt
+++ src/test/kotlin/ControlFlow.kt
@@ -13,7 +13,20 @@ class ControlFlow {
     private fun max(num1: Int, num2: Int): Int = /* return 타입 */ if (num1 > num2) num1 else num2
 
     // when
+    @Test
+    fun `when`() {
 
+    }
+
+    private fun getNumberType(number: Int): String {
+        return when { // whend은 else 브렌치 필요
+            // 만약에 number가 0보다 작으면
+            number < 0 -> "negative"
+            number == 0 -> "zero"
+            number > 0 -> "positive"
+            else -> "error"
+        }
+    }
 
     // while
 
