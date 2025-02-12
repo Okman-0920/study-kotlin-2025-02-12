d20b2a9d830456cba3cb80afbff1a826519e99d7
Okman-0920 <nohoksun94@gmail.com>
2025-02-12 11:27:51 +0900

    ìwhile setting

--- src/test/kotlin/ControlFlow.kt
+++ src/test/kotlin/ControlFlow.kt
@@ -12,7 +12,7 @@ class ControlFlow {
 
     private fun max(num1: Int, num2: Int): Int = /* return 타입 */ if (num1 > num2) num1 else num2
 
-    // when
+    // when - if랑 비슷하지만 가독성을 고려하여 사용한다, switch와 비슷함..
     @Test
     fun `when`() {
         val numberType1 = getNumberType(10)
@@ -35,6 +35,16 @@ class ControlFlow {
     }
 
     // while
+    @Test
+    fun `while`() {
+        val count = 0
+        var counter = 0
+
+        while (count < 5) {
+
+        }
+    }
+
 
 
     // for
