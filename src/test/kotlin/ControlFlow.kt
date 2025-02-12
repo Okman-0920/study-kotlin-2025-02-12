1bc90882e901d3afbbd15ab689560cd8bea3b995
Okman-0920 <nohoksun94@gmail.com>
2025-02-12 11:25:24 +0900

    when 2

--- src/test/kotlin/ControlFlow.kt
+++ src/test/kotlin/ControlFlow.kt
@@ -15,17 +15,23 @@ class ControlFlow {
     // when
     @Test
     fun `when`() {
+        val numberType1 = getNumberType(10)
+        assertEquals("positive", numberType1)
 
+        val numberType2 = getNumberType(0)
+        assertEquals("zero", numberType2)
+
+        val numberType3 = getNumberType(-1)
+        assertEquals("negative", numberType3)
     }
 
-    private fun getNumberType(number: Int): String {
-        return when { // whend은 else 브렌치 필요
-            // 만약에 number가 0보다 작으면
-            number < 0 -> "negative"
-            number == 0 -> "zero"
-            number > 0 -> "positive"
-            else -> "error"
-        }
+    private fun getNumberType(number: Int): String =
+        when { // whend은 else 브렌치 필요
+        // 만약에 number가 0보다 작으면
+        number < 0 -> "negative"
+        number == 0 -> "zero"
+        number > 0 -> "positive"
+        else -> "error"
     }
 
     // while
