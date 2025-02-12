13bf5900d6fee7337d4605b9892009fea526167c
Okman-0920 <nohoksun94@gmail.com>
2025-02-12 10:48:40 +0900

    ìTypeInfer

---
+++ src/test/kotlin/TypeInference.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test
+
+class TypeInference {
+    @Test
+    fun typeInfer() {
+        val str = "kotlin"
+        val age = 30
+
+        // 불린 타입을 추론
+        val bool = true // or false
+
+        // 불린 타입을 추론
+        val double = 3.14
+    }
+}
